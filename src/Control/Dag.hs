cdeeea286f0fa4f3f9327db7e97ff7376b4f0f44
Scott Sadler <github@mashi.org>
2014-11-14 02:14:41 +0100

    of course forgot to commit the main file

--- src/Control/Dag.hs
+++ src/Control/Dag.hs
@@ -6,50 +6,32 @@
 
 
 module Control.Dag
-    ( Cont
-    , Dag
-    , demo
+    ( demo
     ) where
 
 
-import           Control.Monad
-import           Control.Monad.State
-import           Data.Dynamic
+import           Control.Monad.IO.Class
 
-import Control.Dag.Node.ContNode
-import Control.Dag.Node.StateNode
-import Control.Dag.Types.Node
+import           Control.Dag.Node.JunctionNode
+import           Control.Dag.Node.StateNode
+import           Control.Dag.Types.Node
 
-
-noDupes :: (Monad m, Eq a) => (a -> Cont a m ()) -> a -> Cont a m ()
-noDupes f previous = do
-    current <- await
-    when (current /= previous) (f current)
-    noDupes f current
+import           Control.Dag.Prelude
 
 
-fizzBuzz :: (Node n a m, TypeableNode n a m, MonadIO m) => StateNodeT m (StateNode n a (t m))
-fizzBuzz = do
-    printer <- addCont $ noDupes (liftIO . putStr) "_"
-    let fizzybuzzy x s n = when (mod n x == 0) (send printer s)
-    fizz <- addSimple $ fizzybuzzy 3 "Fizz"
-    buzz <- addSimple $ fizzybuzzy 5 "Buzz"
-    return undefined
-    -- addSimple $ \n -> do
-    --     send fizz n
-    --     send buzz n
-    --     send printer "\n"
-  where
-    forever_ :: Monad m => m a -> m ()
-    forever_ = forever
-    addCont :: MonadState DagState m => Cont a m () -> m (StateNode (Cont a m ()) a (t m))
-    addCont = addNode . Ready
 
-    addSimple f = addCont $ forever_ $ await >>= f
+data PrinterNode g = PrinterNode g
+instance (Functor m, Monad m, MonadIO m, Show i) => Node i (PrinterNode i) m where
+    send _ = liftIO . print
+
+
+
+type Inputs = Many2 String ()
+
 
 
 demo :: IO ()
-demo = do
-    (fb, st) <- runStateNodeT fizzBuzz emptyDag
-    resultDag <- foldM (\a b -> execStateNodeT (send fb b) a) st [1..100]
-    print resultDag
+demo = flip evalNodeState emptyDag $ do
+    combiner <- addStateNode $ junction2 $ PrinterNode (undefined::Inputs)
+    send combiner $ OneOf2a "Hello"
+    send combiner $ OneOf2b ()
