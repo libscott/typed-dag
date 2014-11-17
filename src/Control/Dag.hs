f167cf59605f9dbda4546422b6db37d80379a65a
Scott Sadler <github@mashi.org>
2014-11-17 18:15:59 +0100

    just lolling at how simple this is and how it does everything I needed any iteration so far to do

--- src/Control/Dag.hs
+++ src/Control/Dag.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE TypeSynonymInstances       #-}
+{-# LANGUAGE KindSignatures             #-}
 
 
 module Control.Dag
@@ -10,43 +11,20 @@ module Control.Dag
     ) where
 
 
-import           Control.Dag.Types.Pull
-import           Control.Monad.State.Strict
+import           Control.Applicative
 
 
-data A = A
-
-data B a b = B a b
-
-
-instance (Monad m) => Pull2 A Int String m where
-    execute2 _ fa fb = fa 1 >> fb "String"
+data N n o m = N n (n -> m o)
+exec :: N n o m -> m o
+exec (N n f) = f n
 
 
-class (Pull1 n o m) => Collect1 n o m where
-    collect1 :: n -> m o
-
-
-instance (Pull1 n o m) => Collect1 n (Maybe o) m where
-    collect1 node = execStateT (execute1 node put) (error "No output")
-
-
-
-
-
-instance (Collect1 a Int m, Collect1 b String m, Functor m, Monad m) => Pull1 (B a b) Bool m
-  where
-    execute1 (B up1 up2) f = do
-        int <- collect1 up1
-        str <- collect1 up2
-        f $ show int == str
-
-
-
-dag = let (up1, up2) = pull2 A
-       in B up1 up2
-
+dag :: (Applicative m, Functor m, Monad m) => N () String m
+dag = let n2in = N () (\_ -> return ("World", "Hello "))
+          n1left = N () (\_ -> fst <$> exec n2in)
+          n1right = N () (\_ -> snd <$> exec n2in)
+      in N () (\_ -> (++) <$> exec n1right <*> exec n1left)
 
 
 demo :: IO ()
-demo = undefined
+demo = exec dag >>= print
