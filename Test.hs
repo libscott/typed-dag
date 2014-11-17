5d9f7e2917d631d1020211384bf1ef0c247fec4a
Scott Sadler <github@mashi.org>
2014-11-17 12:00:47 +0100

    one more commit, start refactor towards removing typeclasses

--- Test.hs
+++ Test.hs
@@ -3,62 +3,42 @@
 {-# LANGUAGE FlexibleInstances      #-}
 {-# LANGUAGE FlexibleContexts       #-}
 {-# LANGUAGE KindSignatures         #-}
-{-# LANGUAGE FunctionalDependencies #-}
 
 
 
-
--- class BiEmitter o a b where
---     emit2 :: o -> (a, b)
---
--- data HasTwo a b = HasTwo a b
--- instance BiEmitter (HasTwo a b) a b where
---     emit2 (HasTwo a b) = (a, b)
---
--- class UniEmitter o a where
---     emit1 :: o -> a
---
--- data LeftProxy n = LeftProxy n
---
--- instance UniEmitter (LeftProxy a) a where
---     emit1 LeftProxy
-
-
--- these are your new friends.
-
-
-data OutputProxy2 n oa ob (m :: * -> *) = OutputProxy2 n
+data OutputProxy2 oa ob (m :: * -> *) = OutputProxy ((oa -> m ()) -> m ())
 
 
 
-class (Functor m, Monad m) => PullNode1 n o (m :: * -> *) | n -> o
+class (Functor m, Monad m) => PullNode1 n o (m :: * -> *)
   where
       pull1 :: n -> n
       pull1 = id
       execute1 :: n -> (o -> m ()) -> m ()
 
 
-class (Functor m, Monad m) => PullNode2 n oa ob (m :: * -> *) | n -> oa, n -> ob
+class (Functor m, Monad m) => PullNode2 n oa ob (m :: * -> *)
   where
-    pull2 :: n -> (OutputProxy2 n oa ob m, OutputProxy2 n ob oa m)
-    pull2 node = (OutputProxy2 node, OutputProxy2 node)
+    pull2 :: n -> ( OutputProxy2 oa ob m
+                  , OutputProxy2 ob oa m
+                  )
+    pull2 node = ( OutputProxy (\f -> execute2 node (f, \_ -> return ()))
+                 , OutputProxy (\f -> execute2 node (\_ -> return (), f))
+                 )
     execute2 :: n -> (oa -> m (), ob -> m ()) -> m ()
 
 
-instance (Functor m, Monad m, PullNode2 n oa ob m) =>
-         PullNode1 (OutputProxy2 n oa ob m) oa m where
-    execute1 (OutputProxy2 n) f = execute2 n (f, \_ -> return ())
 
-
-instance (Functor m, Monad m, PullNode2 n oa ob m) =>
-         PullNode1 (OutputProxy2 n oa ob m) ob m where
-    execute1 (OutputProxy2 n) f = execute2 n (\_ -> return (), f)
+instance (Functor m, Monad m) => PullNode1 (OutputProxy2 oa ob m) oa m where
+    execute1 (OutputProxy f) = f
 
 
 data EvenOdd u = EvenOdd u
 
 
 instance PullNode1 u Int IO => PullNode2 (EvenOdd u) Int String IO where
+
+
     execute2 (EvenOdd u) (cb1, cb2) = execute1 u $
         \n -> if even n then cb1 n else cb2 (show n)
 
--- src/Control/Dag.hs
+++ src/Control/Dag.hs
@@ -10,93 +10,43 @@ module Control.Dag
     ) where
 
 
-import           Control.Monad.IO.Class
+import           Control.Dag.Types.Pull
 import           Control.Monad.State.Strict
-import           Data.Many
-import           System.IO
-
--- import           Control.Dag.Node.EmitterNode
--- import           Control.Dag.Node.GitNode
--- import           Control.Dag.Node.JobNode
--- import           Control.Dag.Test.Inputs
--- import           Control.Dag.Types
 
 
-type Dag s m = StateT (s m) m
+data A = A
+
+data B a b = B a b
 
 
-
--- | Input nodes
-node0 :: n -> (n, ())
-node0 n = (n, ())
-node1 :: n -> a -> (n, a)
-node1 n a = (n, a)
-node2 :: n -> a -> b -> (n, (a, b))
-node2 n a b = (n, (a, b))
+instance (Monad m) => Pull2 A Int String m where
+    execute2 _ fa fb = fa 1 >> fb "String"
 
 
-buildDag :: n -> Int
-buildDag = undefined
+class (Pull1 n o m) => Collect1 n o m where
+    collect1 :: n -> m o
+
+
+instance (Pull1 n o m) => Collect1 n (Maybe o) m where
+    collect1 node = execStateT (execute1 node put) (error "No output")
 
 
 
 
-data Pull node upstreams = Pull node upstreams
 
-
--- the magic is that the typeclass should be able to figure out
--- automatically how to return the outputs tuple based on the instances.
--- the output tuples are single output Pull nodes which call the parent with a
--- set of callbacks.
---
--- The structure itself is pure, even to create.
--- The GIT pull node is an additional typeclass which allows the structure to
--- be walked and nodes indexed by their name.
--- Perhaps it would even be possible for the walk function to be genericized.
-
--- o: output callbacks
--- u: upstreams
-class (Functor m, Monad m) => PullNode n o (m :: * -> *)
+instance (Collect1 a Int m, Collect1 b String m, Functor m, Monad m) => Pull1 (B a b) Bool m
   where
-      execute :: n -> o -> m ()
-      pull :: n -> o
+    execute1 (B up1 up2) f = do
+        int <- collect1 up1
+        str <- collect1 up2
+        f $ show int == str
 
 
 
-instance PullNode (Pull n (a, b) m) (a, b) m where
-    -- return a set of output proxies corresponding to (a, b)
-    pull n upstreams = ()
+dag = let (up1, up2) = pull2 A
+       in B up1 up2
 
 
 
-data OutputSelector a = OutputSelector a
-
-
-
-
-dag :: Dag MySubscribers IO ()
-dag = do
-    -- dag builder outputs nodes wrapped in address container.
-    let jobNode = Pull InputNode ()
-        (gn1, gn2) = Pull (GitNode "abc" jobNode) ()
-        outputNode = pull1 PrinterNode gitNode
-    runDag outputNode
-
-
-
-data InputNode = InputNode
-instance PullNode (InputNode o) o m where
-    -- return a tuple of nodes which can be called to produce output.
-    pull node upstreams =
-
-
-
-data MyJobNode o = MyJobNode o
-instance PullNode (MyJobNode o) o m where
-    -- return a tuple of nodes which can be called to produce output.
-    pull node = (OutputNode node, OutputNode node)
-
-
-data PrinterNode i = PrinterNode
-instance (Functor m, MonadIO m, Show i) => Node i (PrinterNode i) m where
-    send _ = liftIO . print
+demo :: IO ()
+demo = undefined
